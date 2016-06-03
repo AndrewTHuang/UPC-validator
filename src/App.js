93cfbad3dd580425e2912cf4983fba86a77d9c1e
Andrew Huang <andrew.huang12@gmail.com>
2016-06-03 14:25:02 -0700

    Create responsive InvalidBox component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ValidBox from './components/ValidBox';
+import InvalidBox from './components/InvalidBox';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='app-container'>
+        <InvalidBox />
         <ValidBox />
       </div>
     );
