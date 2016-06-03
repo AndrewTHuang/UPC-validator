913b58b51f34be7c1d70dda9849fa3ddc409e6c9
Andrew Huang <andrew.huang12@gmail.com>
2016-06-03 14:50:57 -0700

    Create InputForm component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import ValidBox from './components/ValidBox';
+import InputForm from './components/InputForm';
 import InvalidBox from './components/InvalidBox';
+import ValidBox from './components/ValidBox';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className='app-container'>
+        <InputForm />
         <InvalidBox />
         <ValidBox />
       </div>
