4bad12985ab0cc095962fd769c9f5d842d6cbc51
Andrew Huang <andrew.huang12@gmail.com>
2016-06-04 13:28:02 -0700

    Map over codes collection to render one per line

--- src/components/ValidBox.js
+++ src/components/ValidBox.js
@@ -3,11 +3,17 @@ import styles from '../styles/styles.scss';
 
 export default class ValidBox extends React.Component {
   render() {
+    const codes = this.props.codes.map((code, index) => {
+      return (
+        <p key={index}>{code}</p>
+      )
+    })
+
     return (
       <div className='valid-box-container'>
         <h1> Valid </h1>
         <div className='valid-box'>
-          {this.props.codes}
+          {codes}
         </div>
       </div>
     );
