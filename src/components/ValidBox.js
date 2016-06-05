0a479ffcf8383054cf287124cb523b299214aad2
Andrew Huang <andrew.huang12@gmail.com>
2016-06-05 13:42:21 -0700

    POST valid codes to server on submit

--- src/components/ValidBox.js
+++ src/components/ValidBox.js
@@ -2,6 +2,35 @@ import React from 'react';
 import styles from '../styles/styles.scss';
 
 export default class ValidBox extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitValidCodes = this.submitValidCodes.bind(this);
+  }
+
+  submitValidCodes() {
+    const body = JSON.stringify({
+      'list': this.props.codes
+    });
+
+    fetch('https://iwo3uesa6c.execute-api.us-east-1.amazonaws.com/prod/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body
+    })
+    .then(res => {
+      if (res.ok) {
+        console.log('UPC codes successfully submitted!');
+      } else {
+        console.log('Uh oh, something went wrong. Got status code ' + res.status);
+      }
+    })
+    .catch(err => {
+      console.log('An error occurred: ', err);
+    })
+  }
+
   render() {
     const codes = this.props.codes.map((code, index) => {
       return (
@@ -15,6 +44,7 @@ export default class ValidBox extends React.Component {
         <div className='valid-box'>
           {codes}
         </div>
+        <button onClick={this.submitValidCodes}> Submit Valid Codes </button>
       </div>
     );
   }
